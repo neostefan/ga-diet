dbb0aca4db3ffb9875428b20901378f2fb6c091d
neostefan <stephen.adingup@gmail.com>
2022-08-31 11:30:33 +0100

    module restructuring

--- nsga_runner_test/nsga_runner_test.go
+++ nsga_runner_test/nsga_runner_test.go
@@ -1,11 +1,13 @@
 package nsgarunner_test
 
-// import (
-// 	"testing"
-
-// 	"github.com/neostefan/diet-assistant/GA"
-// )
-
+//! Look into making a mock db and file and linking it to this function
 // func TestAlgoRunner(t *testing.T) {
-// 	GA.RunAlgorithm()
+// 	objectiveA := "calories"
+// 	objectiveB := "prices"
+
+// 	population := GA.RunAlgorithm(objectiveA, objectiveB)
+
+// 	if len(population) != definitions.PopulationSize {
+// 		t.Errorf("Error with the population length got %v and not %v", len(population), definitions.PopulationSize)
+// 	}
 // }
